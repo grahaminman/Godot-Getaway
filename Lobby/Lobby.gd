b6316c1eecd989dca61df8a618ef137f5223f1e5
Graham Inman <graham.inman@outlook.com>
2020-01-15 17:57:21 +0000

    9 Using a save file
    Vids 8 - 9

--- Lobby/Lobby.gd
+++ Lobby/Lobby.gd
@@ -1,5 +1,8 @@
 extends Control
 
+func _ready():
+	$VBoxContainer/CenterContainer/GridContainer/NameTextBox.text = Saved.save_data["Player_name"]
+
 func _on_HostButton_pressed() -> void:
 	Network.create_server()
 
