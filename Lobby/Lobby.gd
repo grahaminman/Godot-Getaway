e55e633625aeba66e4ba8f413df57696c545551b
Graham Inman <graham.inman@outlook.com>
2020-01-15 18:40:00 +0000

    Vid 10 Populating the waiting room

--- Lobby/Lobby.gd
+++ Lobby/Lobby.gd
@@ -5,15 +5,20 @@ onready var NameTextBox = $VBoxContainer/CenterContainer/GridContainer/NameTextB
 func _ready():
 	NameTextBox.text = Saved.save_data["Player_name"]
 
+
 func _on_HostButton_pressed() -> void:
 	Network.create_server()
-
-
+	create_waiting_room()
 
 func _on_JoinButton_pressed() -> void:
 	Network.connect_to_server()
-
+	create_waiting_room()
 
 func _on_NameTextBox_text_changed(new_text: String) -> void:
 	Saved.save_data["Player_name"] = NameTextBox.text
 	Saved.save_game()
+
+
+func create_waiting_room():
+	$WaitingRoom.popup_centered()
+	$WaitingRoom.refresh_players(Network.players)
