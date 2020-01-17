43e237c550935fd9b466cd744343b8a523a0db78
Graham Inman <ginman.uk@gmail.com>
2020-01-17 14:32:56 +0000

    Vid 36 Scaffolding
    Audio bus - doppler - Fixed miss named GradMap.gd to GirdMap.gd

--- World/ObjectSpawner.gd
+++ World/ObjectSpawner.gd
@@ -9,6 +9,7 @@ var number_of_traffic_cones = 40
 var number_of_hydrants = 50
 var number_of_streetlights = 50
 var number_of_ramps = 25
+var number_of_scaffolding = 25
 
 func generate_props(tile_list, size):
 	tiles = tile_list
@@ -18,6 +19,7 @@ func generate_props(tile_list, size):
 	place_traffic_cones()
 	place_hydrants()
 	place_streetlights()
+	place_scaffolding()
 
 
 func random_tile(tile_count):
@@ -44,12 +46,12 @@ func place_cars():
 		tile_list.pop_front()
 	place_ramps(tile_list)
 
-
 sync func spawn_cars(tile, car_rotation):
 	var car = preload("res://Props/ParkedCars.tscn").instance()
 	car.translation = Vector3( (tile.x * 20) + 10, tile.y, (tile.z * 20)+10)
 	car.rotation_degrees.y = car_rotation
 	add_child(car, true)
+
 
 func place_ramps(tile_list):
 	for i in range(number_of_ramps):
@@ -143,3 +145,22 @@ sync func spawn_streetlights(tile, streetlight_rotation):
 	streetlight.translation = Vector3( (tile.x * 20) + 10, tile.y, (tile.z * 20)+10)
 	streetlight.rotation_degrees.y = streetlight_rotation
 	add_child(streetlight, true)
+
+
+func place_scaffolding():
+	var tile_list = random_tile(number_of_scaffolding)
+	for i in range(number_of_scaffolding):
+		var tile = tile_list[0]
+		var tile_type = get_parent().get_cell_item(tile.x, 0, tile.z)
+		var allowed_rotations = $ObjectRotLookup.lookup_rotation(tile_type)
+		if not allowed_rotations == null:
+			var tile_rotation = allowed_rotations[randi() % allowed_rotations.size() -1] *-1
+			tile.y = tile.y + 0.5
+			rpc("spawn_scaffolding", tile, tile_rotation)
+		tile_list.pop_front()
+
+sync func spawn_scaffolding(tile, scaffolding_rotation):
+	var scaffolding = preload("res://Props/Scaffolding/Scaffolding.tscn").instance()
+	scaffolding.translation = Vector3( (tile.x * 20) + 10, tile.y, (tile.z * 20) +10)
+	scaffolding.rotation_degrees.y = scaffolding_rotation
+	add_child(scaffolding, true)
