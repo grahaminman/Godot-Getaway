388bdf3503695aef0bffe897a024cdce5c99ec2b
Graham Inman <ginman.uk@gmail.com>
2020-02-19 12:26:44 +0000

    Update object.gd

--- Props/object.gd
+++ Props/object.gd
@@ -6,12 +6,12 @@ func _on_Timer_timeout():
 	queue_free()
 
 
-func _on_body_entered(body: Node) -> void:
+func _on_body_entered(body):
 	if not $AudioStreamPlayer3D.playing:
 		$AudioStreamPlayer3D.play()
 
 
-func _on_sleeping_state_changed() -> void:
+func _on_sleeping_state_changed():
 	if not sleeping and has_finished_spawning:
 		$Timer.start()
 	else:
